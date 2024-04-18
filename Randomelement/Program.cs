cbb60d7472c442e8ec58da192d37056f7d3b1661
Rainer Targama <rainertargama@gmail.com>
2024-04-18 12:14:32 +0300

    hamburgur

---
+++ Randomelement/Program.cs
@@ -0,0 +1,4 @@
+﻿string[] snacks = { "pizza", "hamburgur", "sushi", "watermelon", "ramen" };
+Random rand = new Random();
+int randomindex = rand.Next(snacks.Length);
+Console.WriteLine($"Tonight we eatin' {snacks[randomindex]}.");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿string hello = "supreme Jaanek?";
+int stringlength = hello.Length;
+hello = hello.Trim();
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+//int wordCounter = 1;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+//Console.WriteLine($"word count in {hello}: {wordCounter} words.");
